c3c06852c40d9365b60f2758b46904a8c7b43269
simeonMW <simeonmulima17@gmail.com>
2025-05-19 15:08:14 +0200

    group work

--- api.js
+++ api.js
@@ -23,70 +23,7 @@ fetch('https://fakestoreapi.com/products')
 
     })
 
-    mens.addEventListener('click', () => {
-      productImage.innerHTML = '';
-      data.forEach((data) =>{
-        if(data.category === "men's clothing"){
-          productImage.innerHTML += `<div id="imgContainer">
-          <img src=${data.image} alt=${data.description}>
-          <h3>${data.title}</h3>
-          <h3 class="price">${data.price}</h3>
-          </div>`;
-        }
-      })
-
-      jewels.addEventListener('click', () => {
-        productImage.innerHTML = '';
-        data.forEach((data) =>{
-          if(data.category === "jewelery"){
-            productImage.innerHTML += `<div id="imgContainer">
-            <img src=${data.image} alt=${data.description}>
-            <h3>${data.title}</h3>
-            <h3 class="price">${data.price}</h3>
-            </div>`;
-          }
-        })
+   
     
-      })
- 
-      all.addEventListener('click', () => {
-        productImage.innerHTML = '';
-        data.forEach((data) =>{
-            productImage.innerHTML += `<div id="imgContainer">
-            <img src=${data.image} alt=${data.description}>
-            <h3>${data.title}</h3>
-            <h3 class="price">${data.price}</h3>
-            </div>`;
-        })
-    
-      })
-
-      gadgets.addEventListener('click', () => {
-        productImage.innerHTML = '';
-        data.forEach((data) =>{
-          if(data.category === "electronics"){
-            productImage.innerHTML += `<div id="imgContainer">
-            <img src=${data.image} alt=${data.description}>
-            <h3>${data.title}</h3>
-            <h3 class="price">${data.price}</h3>
-            </div>`;
-          }
-        })
-    
-      })
-
-      womens.addEventListener('click', () => {
-        productImage.innerHTML = '';
-        data.forEach((data) =>{
-          if(data.category === "women's clothing"){
-            productImage.innerHTML += `<div id="imgContainer">
-            <img src=${data.image} alt=${data.description}>
-            <h3>${data.title}</h3>
-            <h3 class="price">${data.price}</h3>
-            </div>`;
-          }
-        })
-    
-      })
-   })
  })
+  
