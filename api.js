240655f96fa2311b72e691e1c85076afee6f33a2
simeonMW <simeonmulima17@gmail.com>
2025-05-19 14:47:54 +0200

    update

--- api.js
+++ api.js
@@ -13,9 +13,12 @@ fetch('https://fakestoreapi.com/products')
     
     data.forEach( product => {
         productImage.innerHTML += `<div id="imgContainer">
-        <img src=${product.image} alt=${product.description}>
+        <img src=${product.image} alt=${product.description} id="pImage">
         <h3>${product.title}</h3>
         <h3 class="price">${product.price}</h3>
+        <img src="Favorite_fill.svg" alt="fav" id="favCon">
+        <button id="toCart">Add to cart</button>
+        <button id="viewDets">View details</button>
         </div>`;
 
     })
