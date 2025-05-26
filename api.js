7abde2edd3d6cee1c181a9981a08e1f8285c4045
simeonMW <simeonmulima17@gmail.com>
2025-05-26 13:07:45 +0200

    Update api.js

--- api.js
+++ api.js
@@ -3,6 +3,20 @@ fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then( data => {
     console.log(data);
+
+    const productImage = document.getElementById('productImage');  
+    const allProducts = document.getElementById('all');
+
+    
+    data.forEach( product => {
+        productImage.innerHTML += `<div id="imgContainer">
+        <img src=${product.image} alt=${product.description} id="pImage">
+        <h3>${product.title}</h3>
+        <h3 class="price">${product.price}</h3>
+        <img src="Favorite_fill.svg" alt="fav" id="favCon">
+        <button id="toCart">Add to cart</button>
+        <button id="viewDets">View details</button>
+        </div>`;
   
     const checkout = document.querySelector('.cart');
     const marker_num = document.querySelector('.marker'); 
@@ -80,19 +94,7 @@ fetch('https://fakestoreapi.com/products')
 
 
 
-    const productImage = document.getElementById('productImage');  
-    const allProducts = document.getElementById('all');
 
-    
-    data.forEach( product => {
-        productImage.innerHTML += `<div id="imgContainer">
-        <img src=${product.image} alt=${product.description} id="pImage">
-        <h3>${product.title}</h3>
-        <h3 class="price">${product.price}</h3>
-        <img src="Favorite_fill.svg" alt="fav" id="favCon">
-        <button id="toCart">Add to cart</button>
-        <button id="viewDets">View details</button>
-        </div>`;
 
     })
     
